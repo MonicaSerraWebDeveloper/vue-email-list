cc7753f11847214f6d826773546301ba3e28b901
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-04-09 16:15:00 +0200

    feat: add the for cycle to generate 10 emails to push in an empty array

--- js/script.js
+++ js/script.js
@@ -3,20 +3,18 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        
+        emailArray: []
       }
     },
     methods: {
-
-        listEmail() {
-          const emailArray = []
-          axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-          .then((response) => {
-              console.log(response.data);
-          })
-        }  
       },
       mounted() {
-          this.listEmail()
-      }
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                const randomEmail = response.data.response
+                this.emailArray.push(randomEmail);
+            })
+        }  
+    }
   }).mount('#app')
